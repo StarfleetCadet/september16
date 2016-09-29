1bd5808ff21c95e9cc3049060347ea0fcbb939bd
AD <meine@email.de>
2016-09-29 13:44:43 +0200

    GuessNumberGame

---
+++ GuessNumberGame/src/guessnumbergame/GuessNumberGame.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guessnumbergame;
+import java.swing.JOptionPane;
+
+/**
+ *
+ * @author aviva
+ */
+public class GuessNumberGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        String input = "";
+        int number = 0;
+        int rndNumber = 0;
+        int trys = 0;
+        boolean guessed = false;
+    }
+    
+}
